e901adabe963090aee6bc458e8d9532b2e53c520
Phil <phetzel89@gmail.com>
2021-08-05 18:27:02 -0700

    Routes

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const texts = require("./routes/api/texts");
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.use("/api/users", users);
+app.use("/api/users", texts);
 
 const port = process.env.PORT || 5000;
 
